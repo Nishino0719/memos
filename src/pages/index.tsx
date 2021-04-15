b420ac5f94028cf78515e570ed9e2be88cde999b
Nishino0719 <nishino0719@icloud.com>
2021-04-15 09:30:37 +0900

    :hammer: add count hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,21 @@
+import { useState } from 'react'
+
 export default function Home() {
+  const [count, setCount] = useState(0)
   return (
     <>
-      <div className="m-10 ">
-        {/* <input
-          type="textarea"
-          autoFocus={true}
-          size={50}
-          className="bg-gray-50"
-        /> */}
-        <textarea
-          name="dada"
-          className={'bg-gray-200'}
-          id=""
-          cols={30}
-          rows={10}
-          autoFocus={true}
-        ></textarea>
+      <div className="w-screen h-screen bg-gray-100 ">
+        <div className="m-2 text-center lg:m-10 md:m-5">
+          <textarea
+            name="dada"
+            id=""
+            placeholder={'メモ'}
+            className={'p-2 text-md border rounded-lg resize w-10/12 h-40'}
+            onChange={(e) => setCount(e.target.value.length)}
+            autoFocus={true}
+          ></textarea>
+          <p className={'ml-10 text-lg font-semibold'}>count: {count}</p>
+        </div>
       </div>
     </>
   )
