23c6ea9cd363419db6beff3ae245634b3eb544ce
Nishino0719 <nishino0719@icloud.com>
2022-04-01 17:58:55 +0900

    🔨  add custom hook function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,39 @@ import { useState } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import toast from 'react-hot-toast'
 import { Notification } from '../component/Notification'
+import useHotkey from '../hooks/useHotkey'
 
 export default function Home() {
   const [value, setValue] = useState<string>('')
-  const [displayCount, setDisplayCount] = useState<number>(0)
-  const [canReset, setCanReset] = useState<boolean>(false)
-
-  const handleCopy = () => {
-    navigator.clipboard.writeText(value)
+  const { key, setKey } = useHotkey()
+  if (key === 'c') {
+    document.getElementById('copy').click()
+    setKey('')
   }
-
-  function customValue(str: string) {
-    setCanReset(true)
-    setValue(str)
-    let replacedString = str.replace(/\s+/g, '')
-    setDisplayCount(replacedString.length)
+  if (key === 'r') {
+    if (value.length != 0) {
+      setValue('')
+    }
+    setKey('')
   }
   return (
     <>
-      <div className="w-screen h-screen bg-gray-100 ">
-        <p className={'text-2xl font-semibold text-center mt-10 '}>
-          現在の文字数: {displayCount}文字
-        </p>
+      <div className="w-screen h-screen bg-gray-200 bg-graph-black bg-size-40 ">
+        <div className="">
+          <p className={'text-4xl font-semibold text-center mt-10 '}>
+            現在の文字数: {value.replace(/\s+/g, '').length}文字
+          </p>
+          <div className="flex justify-center gap-10 mt-4">
+            <div className="">
+              <kbd className=" kbd">c</kbd>
+              でコピー
+            </div>
+            <div className="">
+              <kbd className=" kbd">r</kbd>
+              でリセット
+            </div>
+          </div>
+        </div>
         <div className="pt-4 text-center lg:m-10 md:m-5">
           <textarea
             name="dada"
@@ -33,28 +44,13 @@ export default function Home() {
               'p-5 text-md lg:text-lg border rounded-lg resize-none w-10/12 h-96 lg:h-textarea shadow-lg select-all'
             }
             value={value}
-            onChange={(e) => customValue(e.target.value)}
+            onChange={(e) => setValue(e.target.value)}
             autoFocus={true}
           ></textarea>
         </div>
         <div className="grid grid-cols-1 text-center md:grid-cols-2 ">
-          <button
-            id="reset"
-            className={`font-nomal mx-20 my-5 rounded-md p-4 bg-white ${
-              displayCount == 0
-                ? ' shadow-inner cursor-not-allowed'
-                : ' shadow-md transform hover:scale-110 duration-300'
-            }`}
-            onClick={() => {
-              if (displayCount != 0) {
-                setValue('')
-                setDisplayCount(0)
-                setCanReset(false)
-              }
-            }}
-            disabled={canReset}
-          >
-            RESET
+          <button id="reset" className="hidden">
+            reset
           </button>
           <CopyToClipboard
             onCopy={() => {
@@ -62,16 +58,8 @@ export default function Home() {
             }}
             text={value}
           >
-            <button
-              id="copy"
-              className={`font-nomal mx-20 my-5 rounded-md p-4 bg-white ${
-                displayCount == 0
-                  ? ' shadow-inner cursor-not-allowed'
-                  : ' shadow-md transform hover:scale-110 duration-300'
-              }`}
-              disabled={!canReset}
-            >
-              COPY
+            <button id="copy" className="hidden">
+              copy
             </button>
           </CopyToClipboard>
         </div>
